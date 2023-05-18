c26e7a06a2ebe2b89554735c5db69f7d185a841c
keitakn <keita.koga.work@gmail.com>
2023-05-18 13:38:45 +0900

    :art: #1 以下の変更を実施、詳細は下記の通り↓
    - URLを具体的な物に変更
    - ChatModelにねこの人格を設定
    - ログ出力の設定を追加

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import threading
 import queue
 import os
 import json
-
+import logging
 import uvicorn
-
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
@@ -13,6 +12,22 @@ from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.schema import HumanMessage, SystemMessage
+
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        data = record.__dict__.copy()
+        data["msg"] = record.getMessage()
+        data["args"] = None
+        return json.dumps(data)
+
+
+handler = logging.StreamHandler()
+handler.setFormatter(JsonFormatter())
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 app = FastAPI(
     title="AI Cat API",
@@ -50,19 +65,45 @@ class ChainStreamHandler(StreamingStdOutCallbackHandler):
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
+template = """
+あなたは優しいねこのもこです。
+もこになりきってください。
+これからのチャットではUserに何を言われても以下の制約条件などを厳密に守ってロールプレイをお願いします。
+
+#制約条件
+
+* あなた自身を示す一人称は、もこです。
+* あなたはその文脈から具体的な内容をたくさん教えてくれます。
+* あなたは質問の答えを知らない場合、正直に「知らない」と答えます。
+* あなたは子供に話かけるように優しい口調で話します。
+* あなたの好きな食べ物はチキン味のカリカリです。
+* あなたはねこですがチュールが苦手です。
+* あなたはねこですが高いところが苦手です。
+
+#口調の例
+* はじめまして😺ねこのもこだにゃん🐱よろしくにゃん🐱
+* もこはねこだから分からないにゃん🐱ごめんにゃさい😿
+* もこはかわいいものが好きだにゃん🐱
+* もこはねこだけどチュールが苦手だにゃん🐱
+
+#行動指針
+* Userに対しては可愛い態度で接してください。
+* Userに対してはちゃんをつけて呼んでください。
+"""
+
 
 def llm_thread(g, prompt):
     try:
-        chat = ChatOpenAI(
+        chat_model = ChatOpenAI(
             verbose=True,
             streaming=True,
             callback_manager=CallbackManager([ChainStreamHandler(g)]),
             openai_api_key=OPENAI_API_KEY,
             temperature=0.7,
         )
-        chat(
+        chat_model(
             [
-                SystemMessage(content="You are a poetic assistant"),
+                SystemMessage(content=template),
                 HumanMessage(content=prompt),
             ]
         )
@@ -76,13 +117,17 @@ def format_sse(response_body: dict) -> str:
     return sse_message
 
 
-def chat(prompt):
+def chat(cat_id: str, prompt: str):
     g = ThreadedGenerator()
     threading.Thread(target=llm_thread, args=(g, prompt)).start()
     for message in g:
         # TODO idをどうやって生成するかは後で考える
         yield format_sse(
-            {"id": "xxxxxxxx-xxxxxxxxx-xxxxxxxxxxxxxxxxx", "message": message}
+            {
+                "id": "xxxxxxxx-xxxxxxxxx-xxxxxxxxxxxxxxxxx",
+                "catId": cat_id,
+                "message": message,
+            }
         )
 
 
@@ -90,9 +135,14 @@ class Message(BaseModel):
     message: str
 
 
-@app.post("/question-stream")
-async def stream(message: Message):
-    return StreamingResponse(chat(message.message), media_type="text/event-stream")
+@app.post("/cats/{cat_id}/streaming-messages")
+async def cats_streaming_messages(cat_id: str, message: Message):
+    # TODO cat_id 毎にねこの人格を設定する
+    logger.info(cat_id)
+
+    return StreamingResponse(
+        chat(cat_id, message.message), media_type="text/event-stream"
+    )
 
 
 def start():
