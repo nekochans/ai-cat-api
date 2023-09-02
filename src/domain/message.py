7a092ae217a71b31ff17a79359afbd3cb59c169f
keitakn <keita.koga.work@gmail.com>
2023-09-03 01:10:47 +0900

    :recycle: #41 DBの会話履歴からMessageを作成する処理を別packageに移動

--- src/domain/message.py
+++ src/domain/message.py
@@ -1,2 +1,10 @@
+from typing import Literal, TypedDict
+
+
 def is_message(value: str) -> bool:
     return 2 <= len(value) <= 5000
+
+
+class ChatMessage(TypedDict):
+    role: Literal["system", "user", "assistant"]
+    content: str
