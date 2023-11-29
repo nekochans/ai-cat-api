50291f29a00b291f0e51864a213b2dd705af9759
keitakn <keita.koga.work@gmail.com>
2023-11-29 16:10:28 +0900

    :shirt: #73 typecheckで以下のエラーが出ていたので修正
    src/infrastructure/db.py:11: error: Function is missing a type annotation for one or more arguments  [no-untyped-def]
    Found 1 error in 1 file (checked 33 source files)

--- src/infrastructure/db.py
+++ src/infrastructure/db.py
@@ -8,7 +8,7 @@ ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 ctx.load_verify_locations(cafile=os.getenv("SSL_CERT_PATH"))
 
 
-async def create_db_connection(db_name="") -> Connection:
+async def create_db_connection(db_name: str = "") -> Connection:
     loop = asyncio.get_event_loop()
 
     if os.getenv("IS_TESTING") == "1":
