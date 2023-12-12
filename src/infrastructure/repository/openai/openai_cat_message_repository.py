55832253d88d999d5dfdafff4effd6c0873ff077
keitakn <keita.koga.work@gmail.com>
2023-12-13 01:07:25 +0900

    :art: #89 openai のアップグレードに対応して動作するように修正

--- src/infrastructure/repository/openai/openai_cat_message_repository.py
+++ src/infrastructure/repository/openai/openai_cat_message_repository.py
@@ -1,6 +1,6 @@
 import os
 from typing import AsyncGenerator
-from openai import ChatCompletion
+from openai import AsyncOpenAI
 from domain.repository.cat_message_repository_interface import (
     CatMessageRepositoryInterface,
     GenerateMessageForGuestUserDto,
@@ -11,31 +11,27 @@ from domain.repository.cat_message_repository_interface import (
 class OpenAiCatMessageRepository(CatMessageRepositoryInterface):
     def __init__(self) -> None:
         self.OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+        self.client = AsyncOpenAI(api_key=self.OPENAI_API_KEY)
 
     # TODO: 型は合っているのに型チェックエラーが出る mypy が AsyncGenerator に対応していない可能性がある
     # TODO: https://github.com/nekochans/ai-cat-api/issues/68 で別の型チェックツールを試してみる
     async def generate_message_for_guest_user(  # type: ignore
         self, dto: GenerateMessageForGuestUserDto
     ) -> AsyncGenerator[GenerateMessageForGuestUserResult, None]:
-        response = await ChatCompletion.acreate(
+        response = await self.client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=dto.get("chat_messages"),
             stream=True,
-            api_key=self.OPENAI_API_KEY,
             temperature=0.7,
             user=dto.get("user_id"),
         )  # type: ignore
 
         ai_response_id = ""
         async for chunk in response:
-            chunk_message = (
-                chunk.get("choices")[0]["delta"].get("content")
-                if chunk.get("choices")[0]["delta"].get("content")
-                else ""
-            )
+            chunk_message: str = chunk.choices[0].delta.content if chunk.choices[0].delta.content is not None else ""
 
             if ai_response_id == "":
-                ai_response_id = chunk.get("id")
+                ai_response_id = chunk.id
 
             if chunk_message == "":
                 continue
