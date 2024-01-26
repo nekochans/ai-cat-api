815ca6e20c2ed09119d74e4e0cd8b1df3131512b
keitakn <keita.koga.work@gmail.com>
2024-01-27 01:15:57 +0900

    :sparkles: #91 実用的なFunction callingの実装の為、実際の天気を取得するメソッドを実装

--- src/infrastructure/repository/openai/openai_cat_message_repository.py
+++ src/infrastructure/repository/openai/openai_cat_message_repository.py
@@ -1,5 +1,7 @@
 import os
-from typing import AsyncGenerator, cast, List
+import math
+import httpx
+from typing import AsyncGenerator, cast, List, TypedDict
 from openai import AsyncOpenAI
 from openai.types.chat import ChatCompletionMessageParam
 from domain.repository.cat_message_repository_interface import (
@@ -9,9 +11,16 @@ from domain.repository.cat_message_repository_interface import (
 )
 
 
+class FetchCurrentWeatherResponse(TypedDict):
+    city_name: str
+    description: str
+    temperature: int
+
+
 class OpenAiCatMessageRepository(CatMessageRepositoryInterface):
     def __init__(self) -> None:
         self.OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+        self.OPEN_WEATHER_API_KEY = os.environ["OPEN_WEATHER_API_KEY"]
         self.client = AsyncOpenAI(api_key=self.OPENAI_API_KEY)
 
     # TODO: 型は合っているのに型チェックエラーが出る mypy が AsyncGenerator に対応していない可能性がある
@@ -50,3 +59,37 @@ class OpenAiCatMessageRepository(CatMessageRepositoryInterface):
             }
 
             yield chunk_body
+
+    async def _fetch_current_weather(
+        self, city_name: str = "Tokyo"
+    ) -> FetchCurrentWeatherResponse:
+        async with httpx.AsyncClient() as client:
+            geocoding_response = await client.get(
+                "http://api.openweathermap.org/geo/1.0/direct",
+                params={
+                    "q": city_name + ",jp",
+                    "limit": 1,
+                    "appid": self.OPEN_WEATHER_API_KEY,
+                },
+            )
+            geocoding_list = geocoding_response.json()
+            geocoding = geocoding_list[0]
+            lat, lon = geocoding["lat"], geocoding["lon"]
+
+            current_weather_response = await client.get(
+                "https://api.openweathermap.org/data/2.5/weather",
+                params={
+                    "lat": lat,
+                    "lon": lon,
+                    "units": "metric",
+                    "lang": "ja",
+                    "appid": self.OPEN_WEATHER_API_KEY,
+                },
+            )
+            current_weather = current_weather_response.json()
+
+            return {
+                "city_name": city_name,
+                "description": current_weather["weather"][0]["description"],
+                "temperature": math.floor(current_weather["main"]["temp"]),
+            }
