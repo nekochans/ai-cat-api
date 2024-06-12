ada38efa84b2d58ba6a792c3b6c80c6cf6e04f3e
keitakn <keita.koga.work@gmail.com>
2024-06-13 01:00:56 +0900

    :art: #128 API DOCのリクエストパラメータ、レスポンスに説明が出るように設定

--- src/presentation/router/cats.py
+++ src/presentation/router/cats.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter, status, Request, Depends
+from fastapi import APIRouter, status, Request, Depends, Path
 from fastapi.responses import StreamingResponse
 from fastapi.security import HTTPBasicCredentials
 from presentation.auth import basic_auth
 from presentation.controller.generate_cat_message_for_guest_user_controller import (
     GenerateCatMessageForGuestUserRequestBody,
     GenerateCatMessageForGuestUserController,
+    GenerateCatMessageForGuestUserSuccessResponseBody,
+)
+from presentation.error_response_body import (
+    UnauthorizedError,
+    ValidationErrorBody,
+    UnexpectedErrorBody,
 )
 from domain.cat import CatId
 
@@ -15,11 +21,29 @@ router = APIRouter()
     "/cats/{cat_id}/messages-for-guest-users",
     tags=["cats"],
     status_code=status.HTTP_200_OK,
+    response_model=GenerateCatMessageForGuestUserSuccessResponseBody,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": UnauthorizedError,
+            "description": "Authorization Headerが正常に設定されていない場合のレスポンス。",
+        },
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {
+            "model": ValidationErrorBody,
+            "description": "Validation Error時のレスポンス。",
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "model": UnexpectedErrorBody,
+            "description": "予期せぬErrorが発生した時のレスポンス。",
+        },
+    },
 )
 async def generate_cat_message_for_guest_user(
     request: Request,
-    cat_id: CatId,
     request_body: GenerateCatMessageForGuestUserRequestBody,
+    cat_id: CatId = Path(
+        description="ねこのID .e.g. 'moko'",
+        example="4ae80b0f-2e10-4d0d-938e-2c8b0d7a55a1",
+    ),
     credentials: HTTPBasicCredentials = Depends(basic_auth),
 ) -> StreamingResponse:
     controller = GenerateCatMessageForGuestUserController(cat_id, request_body)
